8232335b71b8d0205d84be3630ef5c7eac73ce43
Jaafar <jaafar@MacBook-Pro-de-Jaafar.local>
2017-01-24 23:46:10 -0800

    custom filters and adjustments

--- app/js/filters.js
+++ app/js/filters.js
@@ -1,1 +1,19 @@
 'use strict';
+
+eventsApp.filter('durations', function() {
+	return function (duration){
+
+		switch(duration){
+			case 1:
+				return "45 minutes";
+			case 2:
+				return "60 minutes";
+			case 3:
+				return "90 minutes";
+			case 4:
+				return "120 minutes";
+		}
+	}
+
+
+})
